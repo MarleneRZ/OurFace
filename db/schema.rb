464911755b4085de04922ba1a0ec5f43d2f8ff5e
mduffy4 <mduffy4@students.towson.edu>
2016-07-10 17:50:11 -0400

    This is a big update
    Added a buch of bootstrap and got the profile working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626222022) do
+ActiveRecord::Schema.define(version: 20160710205117) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160626222022) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "location"
+    t.string   "interests"
+    t.string   "relationship_status"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +44,12 @@ ActiveRecord::Schema.define(version: 20160626222022) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "age"
+    t.string   "interests"
+    t.string   "location"
+    t.date     "birthday"
+    t.string   "website"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
