347e7b68beb8e1c10b8bcaae97514d3dc5f51b4b
Baymax2008 <mgale1@students.towson.edu>
2016-07-10 17:53:28 -0400

    This is an attempt at friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :posts
+  has_many :posts
+
+  has_many :friendships
+  has_many :friends, :through => :friendships
 end
